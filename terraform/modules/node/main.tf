d2f4a0f0cf48b63ca666ed758510573bd1ab2d71
Chris Balmer <git@chrisbalmer.io>
2019-06-11 01:06:10 +0000

    Started integrating the terraform node module.

---
+++ terraform/modules/node/main.tf
@@ -0,0 +1,82 @@
+data "vsphere_datacenter" "node_dc" {
+  name = "${var.vsphere_datacenter}"
+}
+
+data "vsphere_compute_cluster" "node_cluster" {
+  name          = "${var.vsphere_cluster}"
+  datacenter_id = "${data.vsphere_datacenter.node_dc.id}"
+}
+
+data "vsphere_resource_pool" "node_pool" {
+  name          = "${var.vsphere_resource_pool}"
+  datacenter_id = "${data.vsphere_datacenter.node_dc.id}"
+}
+
+data "vsphere_datastore" "node_datastore" {
+  datacenter_id = "${data.vsphere_datacenter.node_dc.id}"
+  name          = "${var.vsphere_datastore}"
+}
+
+data "vsphere_network" "node_network" {
+  datacenter_id = "${data.vsphere_datacenter.node_dc.id}"
+  name          = "${var.vsphere_network}"
+}
+
+data "vsphere_virtual_machine" "node_template" {
+  datacenter_id = "${data.vsphere_datacenter.node_dc.id}"
+  name          = "${var.vsphere_template}"
+}
+
+resource "vsphere_virtual_machine" "nodes" {
+  count = "${var.node_count}"
+  name = "${var.node_prefix}${var.node_name}-${count.index}"
+  datastore_id = "${data.vsphere_datastore.node_datastore.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.node_pool.id}"
+  num_cpus = 2
+  memory = 4096
+  guest_id = "${var.node_guest_id}"
+
+  disk {
+    label            = "${var.node_prefix}${var.node_name}-${count.index}.vmdk"
+    size             = "${var.node_disk_size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.node_template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.node_template.disks.0.thin_provisioned}"
+  }
+
+  network_interface {
+    network_id = "${data.vsphere_network.node_network.id}"
+  }
+
+
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.node_template.id}"
+  }
+
+  extra_config {
+    "guestinfo.cloud-init.config.data" = "${base64encode("${data.template_file.node.*.rendered[count.index]}")}"
+    "guestinfo.cloud-init.data.encoding" = "base64"
+  }
+}
+
+
+data "template_file" "node" {
+  count = "${var.node_count}"
+  template = "${file("${path.module}/files/cloud-config.tpl")}"
+
+  vars {
+    hostname = "${var.node_prefix}${var.node_name}-${count.index}"
+    ip_address = "${var.node_ips[count.index]}"
+    gateway = "${var.node_gateway}"
+    dns = "${jsonencode(split(",", var.node_dns))}"
+    network_interface = "${var.node_network_interface}"
+    initial_key = "${var.node_initial_key}"
+    domain_name = "${var.node_domain_name}"
+  }
+}
+
+resource "vsphere_compute_cluster_vm_anti_affinity_rule" "node_anti_affinity" {
+  name                = "${var.node_prefix}node-anti-affinity"
+  compute_cluster_id  = "${data.vsphere_compute_cluster.node_cluster.id}"
+  virtual_machine_ids = ["${vsphere_virtual_machine.nodes.*.id}"]
+}
---
+++ terraform/modules/node/variables.tf
@@ -0,0 +1,73 @@
+variable "vsphere_datacenter" {
+  description = "The datacenter in vCenter to deploy the node(s) to."
+  default     = "the farm"
+}
+variable "vsphere_cluster" {
+  description = "The cluster in the datacenter to deploy the node(s) to."
+  default     = "operations"
+}
+variable "vsphere_resource_pool" {
+  description = "The resouce pool to deploy the node(s) to."
+  default     = "Resources"
+}
+variable "vsphere_datastore" {
+  description = "The datastore to deploy the node(s) to."
+  default     = "vsanDatastore"
+}
+variable "vsphere_network" {
+  description = "The network to connect the node(s) to."
+  default     = "vlan2119-kubernetes"
+}
+variable "vsphere_template" {
+  description = "The template to use for creating the node(s)."
+  default     = "rancheros-1.5.1"
+}
+
+
+variable "node_count" {
+  description = "How many nodes of this type to create."
+  default     = "3"
+}
+variable "node_prefix" {
+  description = "The prefix for the full node name, i.e. dev, prod, etc"
+  default     = "ops"
+}
+variable "node_name" {
+  description = "The name of the node, i.e. worker, master, etc"
+  default     = "worker"
+}
+variable "node_guest_id" {
+  description = "The guest ID for the virtual machine OS, since this assumes RancherOS, this shouldn't be changed yet."
+  default     = "otherLinux64Guest"
+}
+variable "node_disk_size" {
+  description = "The size of the node disk drive in GB."
+  default     = "64"
+}
+variable "node_ips" {
+  description = "IP addresses to assign to the nodes."
+  default = [
+    "172.21.19.21/24",
+    "172.21.19.22/24",
+    "172.21.19.23/24"
+  ]
+}
+variable "node_gateway" {
+  description = "Gateway for the node."
+  default = "172.21.19.1"
+}
+variable "node_dns" {
+  description = "DNS servers for the node."
+  default = "172.21.14.2,172.21.14.4"
+}
+variable "node_network_interface" {
+  description = "Network interface used by the node."
+  default = "eth0"
+}
+variable "node_initial_key" {
+  description = "The initial SSH key to allow access to the node."
+}
+variable "node_domain_name" {
+  description = "The domain name to assign to the node."
+  default     = "farm.oakops.io"
+}
